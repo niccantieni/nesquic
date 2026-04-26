020a1164c55c2db7f75d9a60de4c048ac87b59b9
Nic Cantieni <nic@n1c.ch>
2026-04-26 10:58:30 +0200

    quiche qlog v1

--- iut/quiche/src/client.rs
+++ iut/quiche/src/client.rs
@@ -50,6 +50,7 @@ impl bin::Client for Client {
         // TODO: here we have to set the CA's certificate
         let mut params = ConnectionParams::default();
         params.settings.alpn = vec![b"perf".to_vec()];
+        params.settings.qlog_dir = self.args.qlog.clone();
 
         let (benchmark, send) = Benchmark::new();
         let Ok(qconn) = quic::connect_with_config(socket, None, &params, benchmark).await else {
